5d3258126f16c9443e281bc5b3292356ec5fe259
bq-hentai <bqliu@iflytek.com>
2018-08-06 18:05:34 +0800

    initial Es5 ver.

--- browser/index.js
+++ browser/index.js
@@ -1,9 +1,17 @@
 /*!
  * browser implementation
  * wip
+ *
+ * @todo 上传快错误处理 增加 retry 以及 maxRetryCount
+ * @todo 增加 pause 和 resume
+ *
+ * 支持 pause 和 resume 的话可能需要 Task 以及 TaskRunner
  */
- window.onload = function () {
-  // 未处理后续动作，待完善
+
+;
+
+(function () {
+  // 上传文件 api
   function uploadFile (data) {
     return fetch('http://localhost:10240/upload', {
       method: 'POST',
@@ -11,101 +19,177 @@
     })
   }
 
+  // 通知服务端现在上传完毕了，可以合并了
   function notifyUploadEnd (fileName) {
     return fetch('http://localhost:10240/concat-caches?fileName=' + fileName, {
       method: 'GET'
     })
   }
 
-  // 处理函数
-  const task = {
-    // 切割文件
-    cutfile (file, chunkSize) {
-      // 向上取整
-      const fileSize = file.size
+  function Task (file, options) {
+    if (!(this instanceof Task)) {
+      throw new Error('Use new Task(config).')
+    }
+
+    var defaultOptions = {
+      chunkSize: 100 * 1024,    // 100KB
+      maxConcurrencyNumber: 4,  // Infinity,
+      overrideFileName: '',
+      needMd5sum: true,
+      uploadApi: null,
+      notifyApi: null
+    }
+
+    this.options = Object.assign({ }, defaultOptions, options)
+    this.file = file
+  }
+
+  Task.prototype = {
+    constructor: Task,
+
+    sliceFile: function () {
+      var task = this
+
+      var fileSize = task.file.size
       if (fileSize === 0) {
         return Promise.reject(new Error('No file'))
       }
 
-      const tasks$ = [ ]
-      let startPosition = 0
-      let count = 0
+      var chunks = [ ]
 
+      // 记录当前的起始位置，以及总共的块数
+      var startPosition = 0
+      var count = 0
+
+      // 循环切分
       while (true) {
-        let endPosition = startPosition + chunkSize
+        var endPosition = startPosition + task.options.chunkSize
 
-        const isLastChunk = endPosition >= fileSize
+        // 调整最后的终止位置
+        var isLastChunk = endPosition >= fileSize
         if (isLastChunk) {
           endPosition = fileSize
         }
 
-        const blob = file.slice(startPosition, endPosition)
+        var blob = task.file.slice(startPosition, endPosition)
 
-        const task$ = task.getMd5sum(blob).then(
+        var chunk = task.getMd5sum(blob).then(
+          // 注意 closure
           (function (chunkIndex) {
             return function (md5sum) {
-              return { blob: blob, chunkIndex: chunkIndex, md5sum: md5sum, fileName: file.name }
+              return { blob: blob, chunkIndex: chunkIndex, md5sum: md5sum, fileName: task.file.name }
             }
           })(count),
-          function () { console.log() }
+          function (err) { throw err }
         )
 
-        tasks$.push(task$)
+        chunks.push(chunk)
 
         count += 1
-        startPosition += chunkSize
+        startPosition += task.options.chunkSize
 
+        // 出去的条件
         if (isLastChunk) {
           break
         }
       }
-      return Promise.all(tasks$)
+      return Promise.all(chunks)
     },
+    // 使用 SparkMD5 来获取 md5 值，但是包装成基于 Promise 的
     getMd5sum (file) {
       return new Promise(function (resolve, reject) {
-        const reader = new FileReader()
+        var reader = new FileReader()
         reader.onerror = function (err) {
           reject(err)
         }
         reader.onload = function (e) {
-          const md5sum = SparkMD5.hashBinary(e.target.result)
+          var md5sum = SparkMD5.hashBinary(e.target.result)
           resolve(md5sum)
         }
         reader.readAsBinaryString(file)
       })
     },
-    // 单个文件上传请求
+    // 单个块上传
     uploadChunk (chunk, type) {
-      const fd = new FormData()
+      var fd = new FormData()
       Object.keys(chunk).forEach((prop) => {
         fd.append(prop, chunk[prop])
       })
-      return uploadFile(fd)
+      return this.options.uploadApi(fd)
     },
-    uploadChunks(chunks, type) {
-      // 先只是并发传
-      return Promise.all(chunks.map(function (chunk) {
-        return task.uploadChunk(chunk)
-      })).then(function () {
-        notifyUploadEnd(chunks[0].fileName)
+    // 批量上传多个块
+    uploadChunks(chunks) {
+      var task = this
+
+      var stateMap = { INITIAL: 0, UPLOADING: 1, SUCCESS: 2, ERROR: 3 }
+
+      var fullChunkState = Object.keys(
+        (new Array(chunks.length)).join(',').split(',')
+      ).reduce((acc, x) => {
+          acc[x] = stateMap.INITIAL
+          return acc
+      }, { })
+
+      // util - 获取当前未上传块
+      const randomGetNonUploadChunkIndex = function () {
+        let index = null
+
+        Object.keys(fullChunkState).some((prop) => {
+          if (fullChunkState[prop] === 0) {
+            index = Number(prop)
+
+            return true
+          }
+        })
+
+        return index
+      }
+
+      var next = function (chunk) {
+        fullChunkState[chunk.chunkIndex] = stateMap.UPLOADING
+        return task.uploadChunk(chunk).then(function () {
+          fullChunkState[chunk.chunkIndex] = stateMap.SUCCESS
+
+          var nextIndex = randomGetNonUploadChunkIndex()
+
+          console.log(chunk.chunkIndex, nextIndex)
+
+          if (nextIndex !== null) {
+            return next(chunks[nextIndex])
+          }
+        })
+      }
+
+      var initialUploadQueue = chunks.slice(0, task.options.maxConcurrencyNumber).map(function (chunk) {
+        return next(chunk)
+      })
+
+      Promise.all(initialUploadQueue).then(function () {
+        task.options.notifyApi(chunks[0].fileName)
       })
     }
   }
 
-  const fileInput = document.querySelector('#file')
+  window.onload = function () {
+    var fileInput = document.querySelector('#file')
 
-  fileInput.addEventListener('change', function (evt) {
-    const file = evt.target.files[0];
-    task.cutfile(file, 100 * 1024).then(function (chunks) {
-      const chunkLength = chunks.length
-      chunks.forEach(function (chunk) {
-        chunk.chunkLength = chunkLength
+    fileInput.addEventListener('change', function (evt) {
+      var file = evt.target.files[0]
+
+      var task = new Task(file, { uploadApi: uploadFile, notifyApi: notifyUploadEnd })
+
+      evt.target.value = null
+
+      task.sliceFile().then(function (chunks) {
+        var chunkLength = chunks.length
+        chunks.forEach(function (chunk) {
+          chunk.chunkLength = chunkLength
+        })
+
+        console.log(chunks)
+
+        return task.uploadChunks(chunks)
       })
-
-      console.log(chunks)
-
-      return task.uploadChunks(chunks)
     })
-  })
- }
-
+  }
+})()
